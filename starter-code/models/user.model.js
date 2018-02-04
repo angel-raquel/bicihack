10d3ae9eedcb92eb89a88240246c9fac533d018b
mouyaq <mouyaq@gmail.com>
2018-02-05 00:43:36 +0100

    feat: user model done, fixes #1

---
+++ starter-code/models/user.model.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+require('mongoose-type-email');
+const bcrypt = require('bcrypt');
+const SALT_WORK_FACTOR = 10;
+
+const ADMIN_ROLE = "ADMIN";
+const USER_ROLE = "USER";
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: mongoose.SchemaTypes.Email,
+        required: [true, 'Email is required.'],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required.'],
+        minlength: [8, 'Password must have at least 8 characters.']
+    },
+    role: {
+        type: String,
+        enum: [ADMIN_ROLE, USER_ROLE],
+        default: USER_ROLE
+    },
+    name: {
+        type: String,
+    },
+    cardNumber: {
+        type: Number,
+    },
+    favoriteStations: {
+        type: [Schema.Types.ObjectId]
+    },
+    imageUrl: {
+        type: String,
+        match: '/(\w*\W*)?\w*(\.(\w)+)+(\W\d+)?(\/\w*(\W*\w)*)*/'
+    }
+}, { timestamps: true} );
+
+userSchema.pre('save', function(next) {
+
+    if (!this.isModified('password')) {
+        return next();
+    }
+
+    if (this.isAdmin()) {
+        this.role = ADMIN_ROLE;
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR)
+        .then(salt => {
+            bcrypt.hash(this.password, salt)
+                .then(hash => {
+                    this.password = hash;
+                    next();
+                })
+        })
+        .catch(error => next(error));
+
+});
+
+userSchema.methods.checkPassword = function(password) {
+    return bcrypt.compare(password, this.password);
+}
+    
+userSchema.methods.isAdmin = function() {
+    return this.role === ADMIN_ROLE;
+} 
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
