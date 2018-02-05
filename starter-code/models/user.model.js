ca584ec79b96169b96447b1ac1461653a6c01012
mouyaq <mouyaq@gmail.com>
2018-02-05 18:15:59 +0100

    fix: change email to username in user.model.js

--- starter-code/models/user.model.js
+++ starter-code/models/user.model.js
@@ -6,7 +6,7 @@ const ADMIN_ROLE = "ADMIN";
 const USER_ROLE = "USER";
 
 const userSchema = new mongoose.Schema({
-    email: {
+    username: {
         type: String,
         required: [true, 'Email is required.'],
         match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
