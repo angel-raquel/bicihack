aba1110399b11aa1a8d42fa0058831c0637e8d24
Rak <lovetacirupeca@gmail.com>
2018-02-06 20:22:24 +0100

    feat: add station controller and test stations view

--- starter-code/controllers/station.controller.js
+++ starter-code/controllers/station.controller.js
@@ -1,11 +1,9 @@
 const Station = require('../models/station.model');
 
 module.exports.index = (req, res) => {
-  /*Station.find({}).then((stations) => {
-    res.render("index", {
-      stations: stations,
-      path: req.path
+  Station.find({}).then((stations) => {
+    res.render("stations/index", {
+      stations: stations
     });
-  });*/
-  res.send("HOLA")
+  });
 };
