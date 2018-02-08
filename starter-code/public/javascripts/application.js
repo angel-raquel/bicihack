b32db02c6401e0be260ec8bf4f396863a339ff29
Rak <lovetacirupeca@gmail.com>
2018-02-08 20:10:04 +0100

    feat: ALELUYA! We can see the map but is ugly

---
+++ starter-code/public/javascripts/application.js
@@ -0,0 +1,64 @@
+/*
+var map;
+var currentMarker;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 40.415451988566375, lng: 356.29836363220215},
+    zoom: 14
+  });
+}
+
+function placeStation(latLng) {
+  if (currentMarker !== undefined) {
+    currentMarker.setMap(null);
+  }
+  currentMarker = new google.maps.Marker({
+    position: latLng,
+    map: map
+  });
+}
+*/
+
+
+
+function initialize() {
+            var center = new google.maps.LatLng(40.41, 356.3);
+            var map = new google.maps.Map(document.getElementById('map'), {
+                zoom: 12,
+                center: center,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            });
+            var opt = {
+                "styles" : [
+                    {textColor: "black", textSize: 15, height: 60, width: 60},
+                    {textColor: "black", textSize: 15, height: 70, width: 70},
+                    {textColor: "black", textSize: 15, height: 80, width: 80},
+                    {textColor: "black", textSize: 15, height: 90, width: 90},
+                    {textColor: "black", textSize: 15, height: 100, width: 100}
+                ],
+                "legend": {
+                    //"Broke" : "#FF0066",
+                    //"Bases" : "#FF9933",
+                    "Bicycles anchored" : "#FFFF00" ,
+                    "Free bases" : "#99FF99",
+                    //"No injuries" : "#66CCFF",
+                    "Reservations" : "#A5A5A5"
+                }
+            };
+            var markers = [];
+            for (var i = 0; i < stations.length; i++) {
+
+                var stationLat = stations[i].latitude;
+                var stationLng = stations[i].longitude;
+                var stationLatLng = new google.maps.LatLng(Number(stationLat), Number(stationLng));
+                var marker = new google.maps.Marker({
+                    position: stationLatLng
+                });
+                markers.push(marker);
+            }
+            var markerCluster = new MarkerClusterer(map, markers, opt);
+        }
+
+        //google.load("visualization", "1", {packages: ["corechart"]});
+        //google.setOnLoadCallback(initialize);
