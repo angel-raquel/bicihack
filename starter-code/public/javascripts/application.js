0e0f1444f939a991e4b2343207b78dde10e53f6b
Rak <lovetacirupeca@gmail.com>
2018-02-10 10:30:19 +0100

    feat: put pretty the code in application.js

--- starter-code/public/javascripts/application.js
+++ starter-code/public/javascripts/application.js
@@ -20,51 +20,48 @@ function placeStation(latLng) {
 }
 */
 
-
-
 function initialize() {
-            var center = new google.maps.LatLng(40.41, 356.3);
-            var map = new google.maps.Map(document.getElementById('map'), {
-                zoom: 12,
-                center: center,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            });
+  var center = new google.maps.LatLng(40.41, 356.3);
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: center,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  });
 
-            var opt = {
-                "styles" : [
-                    {textColor: "black", textSize: 15, height: 60, width: 60},
-                    {textColor: "black", textSize: 15, height: 70, width: 70},
-                    {textColor: "black", textSize: 15, height: 80, width: 80},
-                    {textColor: "black", textSize: 15, height: 90, width: 90},
-                    {textColor: "black", textSize: 15, height: 100, width: 100}
-                ],
-                "legend": {
-                    //"Broke" : "#FF0066",
-                    //"Bases" : "#FF9933",
-                    //"Bicycles anchored" : "#FFFF00" ,
-                    "no_available" : "#99FF99",
-                    "available" : "#66CCFF",
-                    //"Reservations" : "#A5A5A5"
-                }
-            };
-            var markers = [];
-            for (var i = 0; i < stations.length; i++) {
-
-                var stationLat = stations[i].latitude;
-                var stationLng = stations[i].longitude;
-                var stationLatLng = new google.maps.LatLng(Number(stationLat), Number(stationLng));
-                var no_available = stations[i].no_available;
-                if (no_available = 1) {
-                  no_available = "no_available"
-                } else {
-                  no_available = "available"
-                }
-                var marker = new google.maps.Marker({
-                    position: stationLatLng,
-                    title: no_available
-                    //no_available: no_available
-                });
-                markers.push(marker);
-            }
-            var markerCluster = new MarkerClusterer(map, markers,opt);
+  var opt = {
+    "styles" : [
+      {textColor: "black", textSize: 15, height: 60, width: 60},
+      {textColor: "black", textSize: 15, height: 70, width: 70},
+      {textColor: "black", textSize: 15, height: 80, width: 80},
+      {textColor: "black", textSize: 15, height: 90, width: 90},
+      {textColor: "black", textSize: 15, height: 100, width: 100}
+    ],
+    "legend": {
+      //"Broke" : "#FF0066",
+      //"Bases" : "#FF9933",
+      //"Bicycles anchored" : "#FFFF00" ,
+      "no_available" : "#99FF99",
+      "available" : "#66CCFF",
+      //"Reservations" : "#A5A5A5"
+    }
+  };
+  var markers = [];
+  for (var i = 0; i < stations.length; i++) {
+    var stationLat = stations[i].latitude;
+    var stationLng = stations[i].longitude;
+    var stationLatLng = new google.maps.LatLng(Number(stationLat), Number(stationLng));
+    var no_available = stations[i].no_available;
+    if (no_available = 1) {
+      no_available = "no_available"
+    } else {
+      no_available = "available"
+    }
+    var marker = new google.maps.Marker({
+      position: stationLatLng,
+      title: no_available
+      //no_available: no_available
+    });
+    markers.push(marker);
+  }
+  var markerCluster = new MarkerClusterer(map, markers,opt);
         }
