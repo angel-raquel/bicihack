898b1d777b2e43c8bb29910c63cf235b4ff5bbb2
Rak <lovetacirupeca@gmail.com>
2018-02-10 11:43:20 +0100

    feat: map with markers information

--- starter-code/public/javascripts/application.js
+++ starter-code/public/javascripts/application.js
@@ -1,4 +1,82 @@
-/*
+// INTERNET
+
+var map;
+var infoWindow;
+
+function initialize() {
+   var mapOptions = {
+      center: new google.maps.LatLng(40.415451988566375,356.29836363220215),
+      zoom: 9,
+      mapTypeId: 'roadmap',
+   };
+
+   map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+
+   // a new Info Window is created
+   infoWindow = new google.maps.InfoWindow();
+
+   // Event that closes the Info Window with a click on the map
+   google.maps.event.addListener(map, 'click', function() {
+      infoWindow.close();
+   });
+
+   // Finally displayMarkers() function is called to begin the markers creation
+   displayMarkers();
+}
+google.maps.event.addDomListener(window, 'load', initialize);
+
+function displayMarkers(){
+
+   // this variable sets the map bounds according to markers position
+   var bounds = new google.maps.LatLngBounds();
+
+   // for loop traverses markersData array calling createMarker function for each marker
+   for (var i = 0; i < stations.length; i++){
+      var latlng = new google.maps.LatLng(stations[i].latitude, stations[i].longitude);
+      var name = stations[i].name;
+      var dock_bikes = stations[i].dock_bikes;
+      var free_bases = stations[i].free_bases;
+      var reservations_count = stations[i].reservations_count;
+
+      createMarker(latlng, name, dock_bikes, free_bases, reservations_count);
+
+      // marker position is added to bounds variable
+      bounds.extend(latlng);
+   }
+
+   // Finally the bounds variable is used to set the map bounds
+   // with fitBounds() function
+   map.fitBounds(bounds);
+}
+
+function createMarker(latlng, name, dock_bikes, free_bases, reservations_count){
+   var marker = new google.maps.Marker({
+      map: map,
+      position: latlng,
+      title: name
+   });
+
+   // This event expects a click on a marker
+   // When this event is fired the Info Window content is created
+   // and the Info Window is opened.
+   google.maps.event.addListener(marker, 'click', function() {
+
+      // Creating the content to be inserted in the infowindow
+      var iwContent = '<div id="iw_container">' +
+            '<div class="iw_title">' + name + '</div>' +
+         '<div class="iw_content">' + dock_bikes + '<br />' +
+         free_bases + '<br />' +
+         reservations_count + '</div></div>';
+
+      // including content to the Info Window.
+      infoWindow.setContent(iwContent);
+
+      // opening the Info Window in the current map and at the current marker location.
+      infoWindow.open(map, marker);
+   });
+}
+/* Julio MAP
+
 var map;
 var currentMarker;
 
@@ -18,8 +96,10 @@ function placeStation(latLng) {
     map: map
   });
 }
+
 */
 
+/* Cluster MAP
 function initialize() {
   var center = new google.maps.LatLng(40.41, 356.3);
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -65,3 +145,4 @@ function initialize() {
   }
   var markerCluster = new MarkerClusterer(map, markers,opt);
         }
+*/
