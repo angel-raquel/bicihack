5a3e6ec81d135fc0975a62729c93b9868cefdf29
mouyaq <mouyaq@gmail.com>
2018-02-17 12:00:09 +0100

    feat: customMarkers on index map

--- starter-code/public/javascripts/application.js
+++ starter-code/public/javascripts/application.js
@@ -1,116 +1,82 @@
-var map;
-var infoWindow;
-
 function initialize() {
-   var mapOptions = {
-      center: new google.maps.LatLng(40.415451988566375,356.29836363220215),
-      zoom: 9,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-   };
+	
+	var mapOptions = {
+		zoom: 14,
+        center: new google.maps.LatLng(40.415451988566375,356.29836363220215),
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+	}
+	
+    var map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    
+    var options = {
+        //title: 'Station info',
+        fontSize: 8,
+        backgroundColor: 'transparent',
+        pieHole: 0.4,
+        colors: ['#00cc00', '#0000ff', '#ff0000'],
+        legend: 'none'
+    };
 
-   map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    var markers = [];
 
-   // a new Info Window is created
-   infoWindow = new google.maps.InfoWindow();
+    for (var i = 0; i < stations.length; i++){
+        var position = new google.maps.LatLng(stations[i].latitude, stations[i].longitude);
+        var data = google.visualization.arrayToDataTable([
+            ['parameter', 'value'],
+            ['bikes', stations[i].dock_bikes],
+            ['docks', stations[i].free_bases],
+            ['reservations', stations[i].reservations_count]
+        ]);
+        
+        markers.push(
+            new CustomMarker(
+                {             
+                    map: map,
+                    position: position,  
+                    width: '50px',
+                    height: '50px',
+                    chartData: data,
+                    chartOptions: options
+                }
+            )
+        );
 
-   // Event that closes the Info Window with a click on the map
-   google.maps.event.addListener(map, 'click', function() {
-      infoWindow.close();
-   });
-
-
-   // Finally displayMarkers() function is called to begin the markers creation
-
-   var markerCluster = new MarkerClusterer(map, displayMarkers(),
-            {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
-
-    var infoLocation = new google.maps.InfoWindow({map: map});
-
-    // Try HTML5 geolocation.
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(function(position) {
-        var pos = {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-        };
-
-        infoLocation.setPosition(pos);
-        infoLocation.setContent(`You're here`);
-        map.setCenter(pos);
-        }, function() {
-        handleLocationError(true, infoLocation, map.getCenter());
-        });
-        // User position
-        var marker = new google.maps.Marker({
-            position: pos,
-            map: map
-          });
-          marker.setMap(map);
-    } else {
-        // Browser doesn't support Geolocation
-        handleLocationError(false, infoLocation, map.getCenter());
+        // var name = stations[i].name;
+        // var dock_bikes = stations[i].dock_bikes;
+        // var free_bases = stations[i].free_bases;
+        // var reservations_count = stations[i].reservations_count;
     }
 
-}
-//google.maps.event.addDomListener(window, 'load', initialize);
+    var markerCluster = new MarkerClusterer(map, markers,
+        {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 
-function displayMarkers(){
-  var markers = [];
-   // this variable sets the map bounds according to markers position
-   var bounds = new google.maps.LatLngBounds();
+        var infoLocation = new google.maps.InfoWindow({map: map});
 
-   // for loop traverses markersData array calling createMarker function for each marker
-   for (var i = 0; i < stations.length; i++){
-      var latlng = new google.maps.LatLng(stations[i].latitude, stations[i].longitude);
-      var name = stations[i].name;
-      var dock_bikes = stations[i].dock_bikes;
-      var free_bases = stations[i].free_bases;
-      var reservations_count = stations[i].reservations_count;
+        // Try HTML5 geolocation.
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+    
+            infoLocation.setPosition(pos);
+            infoLocation.setContent(`You're here`);
+            //map.setCenter(pos);
+            }, function() {
+                handleLocationError(true, infoLocation, map.getCenter());
+            });
+            // User position
+            // var marker = new google.maps.Marker({
+            //     position: pos,
+            //     map: map
+            //   });
+            //   marker.setMap(map);
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoLocation, map.getCenter());
+        }
 
-
-      markers.push(createMarker(latlng, name, dock_bikes, free_bases, reservations_count));
-
-      // marker position is added to bounds variable
-      bounds.extend(latlng);
-   }
-
-   // Finally the bounds variable is used to set the map bounds
-   // with fitBounds() function
-   map.fitBounds(bounds);
-   return markers
-}
-
-function createMarker(latlng, name, dock_bikes, free_bases, reservations_count){
-   var marker = new google.maps.Marker({
-      map: map,
-      position: latlng,
-      title: name
-   });
-
-   // This event expects a click on a marker
-   // When this event is fired the Info Window content is created
-   // and the Info Window is opened.
-   google.maps.event.addListener(marker, 'click', function() {
-
-      // Creating the content to be inserted in the infowindow
-      var iwContent = '<div id="iw_container">' +
-            '<div class="iw_title">' + name + '</div>' +
-         '<div class="iw_content">' + dock_bikes + '<br />' +
-         free_bases + '<br />' +
-         reservations_count + '</div></div>';
-
-      // including content to the Info Window.
-      infoWindow.setContent(iwContent);
-
-      // opening the Info Window in the current map and at the current marker station.
-      infoWindow.open(map, marker);
-   });
-   return marker
 }
 
-function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-  infoWindow.setPosition(pos);
-  infoWindow.setContent(browserHasGeolocation ?
-                          'Error: The Geolocation service failed.' :
-                          'Error: Your browser doesn\'t support geolocation.');
-}
+google.maps.event.addDomListener(window, 'load', initialize);
