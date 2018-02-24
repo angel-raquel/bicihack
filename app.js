111a0b67c3aeca6ad9fd5bc07b4c6b6e50438381
Rak <lovetacirupeca@gmail.com>
2018-02-24 18:48:39 +0100

    feat: add favicon

--- app.js
+++ app.js
@@ -34,15 +34,12 @@ const user = require('./routes/user.routes');
 const station = require('./routes/station.routes');
 const issue = require('./routes/issue.routes');
 
-
-// view engine setup
 app.use(expressLayouts);
 app.set('layout', 'layouts/main');
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(path.join(__dirname, 'public/images/favicon.png')));
+app.use(favicon(path.join(__dirname, '/public/images/', 'favicon.png')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -74,20 +71,16 @@ app.use('/user', user);
 app.use('/station', station);
 app.use('/issue', issue);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
