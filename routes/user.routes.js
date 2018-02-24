934b716120143d35814987d834b8576af9a4011f
Rak <lovetacirupeca@gmail.com>
2018-02-24 19:39:46 +0100

    feat: add cloud service(cloudinary) for avatars

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/user.controller');
+const cloudinary = require('cloudinary');
+const cloudinaryStorage = require('multer-storage-cloudinary');
 const multer  = require('multer');
-const upload = multer({ dest: 'public/user-avatars' });
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: 'user-avatars',
+  allowedFormats: ['jpg', 'png'],
+  filename: function (req, file, cb) {
+    cb(undefined, 'avatar');
+  }
+});
+const parser = multer({ storage: storage });
 const secure = require('../middleware/security.middleware');
 
 router.get('/:id', secure.isMyProfile, userController.profile);
 router.get('/edit/:id', secure.isMyProfile, userController.edit);
-router.post('/edit/:id', [secure.isMyProfile, upload.single('avatar')], userController.doEdit);
+router.post('/edit/:id', [secure.isMyProfile, parser.single('avatar')], userController.doEdit);
 
 
 module.exports = router;
